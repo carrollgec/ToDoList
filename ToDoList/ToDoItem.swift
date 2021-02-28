f3bc8bad37ce4241730ed982c70e922f05142931
Grace <carrolhn@bc.edu>
2021-02-28 16:50:13 -0500

    Completed 5.6

---
+++ ToDoList/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by Grace Carroll on 2/28/21.
+//
+
+import Foundation
+
+struct ToDoItem{
+    var name: String
+    var date: Date
+    var notes: String
+}
